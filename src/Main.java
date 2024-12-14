231a325f8fc14d104c7b5e3de5531f3fa3163e59
ENES <enescebeci01@gmail.com>
2024-12-14 17:47:17 +0300

    aaaaaaaaaaaaaaaaaaaaaaaa

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
-
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {}
+        private String açıklama;
+        private boolean tamamlandı;
+
+        public Main(String aciklama){
+            this.açıklama=aciklama;
+            this.tamamlandı=false;
+        }
+        public String getteraçıklama(){ //getter sınıfın içindekini okumak private sınıflarına erişmek için
+            return açıklama;
+        }
+        public void setteraçıklama(String aciklama){ //setter açıklamayı değiştirmek private sınıfı içindeki örnek
+            this.açıklama=aciklama;
+        }
+        public boolean vazifetamamlandı(){ //işin tamamlanıp tamamlanmadığı için
+            return tamamlandı;
+        }
+        public void settertamamlandı(boolean tamamlandi){ //tamamlanmış olarak işareti değiştirir veya tersi
+            this.tamamlandı=tamamlandi;
+        }
+    public String toString() {
+        return "Görev: " + açıklama + ", Tamamlandı mı: " + (tamamlandı ? "Evet" : "Hayır");
+}
 }
